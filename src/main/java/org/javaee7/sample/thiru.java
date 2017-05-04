fade6d1b43f9aa5d747977bf7e725ba9e22d6088
thirupathy82 <thirupathy82@gmail.com>
2017-05-04 18:13:25 +0530

    Update thiru.java

--- src/main/java/org/javaee7/sample/thiru.java
+++ src/main/java/org/javaee7/sample/thiru.java
@@ -2,7 +2,7 @@ public class thiru {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World");
+        System.out.println("Hello, World - To -Thiru");
     }
 
 }
